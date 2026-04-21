46e4350cb03674c55a6b0f3d013d206b5d1988a9
Larry <larryle7680@gmail.com>
2026-04-21 13:42:45 -0500

    Fixed the arrangement of my code Put the close outside of my loop Finish the payroll calc

--- src/main/java/com/pluralsight/PayRollCalcApp.java
+++ src/main/java/com/pluralsight/PayRollCalcApp.java
@@ -1,21 +1,36 @@
 package com.pluralsight;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.util.Scanner;
+import java.io.*;
 import java.util.Arrays;
 //import java.util.*
 
 public class PayRollCalcApp {
+    static Scanner theScanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         try {
+            //Do this first
+            //Prompt the user questions:
+            System.out.print("Enter the name of the file employee file to process: ");
+            String theFile3 = theScanner.nextLine();
+            System.out.println();
+            System.out.print("Enter the name of the payroll file to create: ");
+            String namePayRoll = theScanner.nextLine();
+
+            //Concat but don't hard code the files
             //Goes to the source of the file
-            FileReader reader = new FileReader("src/main/resources/employees.csv");
+            FileReader reader = new FileReader("src/main/resources/" + theFile3);
             //Actually reads it line by line/pass the reader into the bufReader ro read the actual files
             BufferedReader theFile = new BufferedReader(reader);
 
+            //Creating a file writer
+            FileWriter fileWriter = new FileWriter("src/main/resources/" + namePayRoll);
+            //create a buffered writer
+            BufferedWriter theFile2 = new BufferedWriter(fileWriter);
+
+            //Add Header to the file
+            theFile2.write("ID|NAME|GROSSPAY\n");
             String line;
             int lineNum = 0;
             while((line = theFile.readLine()) != null){
@@ -26,6 +41,8 @@ public class PayRollCalcApp {
                 if(lineNum ==1){
                     continue;
                 }
+
+
 
                 //process the other rows
                 // split the row on the |
@@ -47,9 +64,17 @@ public class PayRollCalcApp {
 
                 Employee employee = new Employee(id, name, employeeHours, employeePayRate);
 
-                System.out.printf("ID: %d\n Name: %s\n Gross pay is %.2f.\n",employee.getEmployeeId(),employee.getName(),employee.getGrossPay()
-                );
+                System.out.printf("ID: %d\n Name: %s\n Gross pay is %.2f.\n",employee.getEmployeeId(),employee.getName(),employee.getGrossPay());
+
+                //Writing the file into theFile2
+                theFile2.write(employee.getEmployeeId() +"|" +employee.getName() +"|"+ employee.getGrossPay() + "\n");
+                //Close the Buffered Writer and reader, so it could empty into the new file
+
             }
+            //Close outside of loop
+            theFile2.close();
+            fileWriter.close();
+
             //Exception e will catch all errors
         } catch (Exception e) {
             System.out.println("Could not find file");
